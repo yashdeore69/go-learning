42dcf77bd0aa92f6a092586aa7a8c85447dcc741
yashdeore69 <deoreyash2003@gmail.com>
2026-02-15 02:06:28 +0530

    added more languages

--- hello_test.go
+++ hello_test.go
@@ -3,15 +3,27 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("saying hello to people", func(t *testing.T){
-		got := Hello("Chris")
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
 		want := ("Hello, Chris")
 		assertCorrectMessage(t, got, want)
 	})
 	
-	t.Run("say 'Hello, World' when an empty string is passed", func(t *testing.T){
-		got := Hello("")
+	t.Run("say 'Hello, World' when an empty string is passed", func(t *testing.T) {
+		got := Hello("", "")
 		want := ("Hello, World")
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Jeremy", "French")
+		want := ("Bonjour, Jeremy")
 		assertCorrectMessage(t, got, want)
 	})
 
--- main.go
+++ main.go
@@ -2,13 +2,24 @@ package main
 
 import "fmt"
 
+const french = "French"
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
-func Hello(name string) string {
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+	if language == french {
+		return frenchHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Jeremy", "Spanish"))
 }
